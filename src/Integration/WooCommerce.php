2fe0162b6e660cea3643289c760aca9420f51753
Anh Tran <anhtnt@elightup.com>
2020-04-11 12:00:20 +0700

    Style WC archive page

--- src/Integration/WooCommerce.php
+++ src/Integration/WooCommerce.php
@@ -9,21 +9,9 @@ class WooCommerce {
 	}
 
 	public function add_theme_support() {
-		add_theme_support( 'woocommerce' );
-
-		// add_theme_support( 'woocommerce', [
-		// 	'thumbnail_image_width' => 150,
-		// 	'single_image_width'    => 300,
-
-		// 	'product_grid'          => [
-		// 		'default_rows'    => 3,
-		// 		'min_rows'        => 2,
-		// 		'max_rows'        => 8,
-		// 		'default_columns' => 4,
-		// 		'min_columns'     => 2,
-		// 		'max_columns'     => 5,
-		// 	],
-		// ] );
+		add_theme_support( 'woocommerce', [
+			'thumbnail_image_width' => 245,
+		] );
 
 		add_theme_support( 'wc-product-gallery-zoom' );
 		add_theme_support( 'wc-product-gallery-lightbox' );
@@ -36,13 +24,13 @@ class WooCommerce {
 		remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end' );
 
 		// No sidebar for single products.
-		if ( is_single() ) {
+		if ( is_product() ) {
 			remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
 		}
 	}
 
 	public function add_body_classes( $classes ) {
-		if ( ! is_singular( 'product' ) ) {
+		if ( ! is_woocommerce() ) {
 			return $classes;
 		}
 		$classes = array_diff( $classes, [
@@ -50,6 +38,9 @@ class WooCommerce {
 			'sidebar-right', 'sidebar-left', 'no-sidebar',
 			'thumbnail-before-header', 'thumbnail-after-header', 'thumbnail-header-background',
 			'entry-header-left', 'entry-header-right', 'entry-header-center',
+
+			'archive',
+			'list-horizontal', 'list-vertical', 'grid', 'grid-card',
 		] );
 		return $classes;
 	}
