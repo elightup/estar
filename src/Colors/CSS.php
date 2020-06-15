04e2a0f1ed998744f1914fccc6de095190a7833f
Anh Tran <anhtnt@elightup.com>
2020-06-15 09:01:24 +0700

    Make custom colors work in the editor

--- src/Colors/CSS.php
+++ src/Colors/CSS.php
@@ -6,10 +6,30 @@ class CSS {
 
 	public function __construct() {
 		$this->elements = Colors::get_elements();
-		add_action( 'wp_head', [ $this, 'output' ] );
+		add_action( 'wp_head', [ $this, 'output_frontend' ] );
+        add_action( 'admin_head', [ $this, 'output_admin' ] );
 	}
 
-	public function output() {
+	public function output_frontend() {
+		$css = $this->get_css();
+
+		// Only include custom colors in customizer or frontend.
+		if ( is_customize_preview() || $css ) {
+			echo "\n<style id='estar-colors'>\n:root {\n\t", esc_html( implode( "\n\t", $css ) ), "\n}\n</style>\n";
+		}
+	}
+
+	public function output_admin() {
+        $screen = get_current_screen();
+		$css = $this->get_css();
+        if ( ! $screen || ! $screen->is_block_editor() || ! $css ) {
+            return;
+        }
+
+		echo "\n<style>\n.editor-styles-wrapper.editor-styles-wrapper {\n\t", esc_html( implode( "\n\t", $css ) ), "\n}\n</style>\n";
+	}
+
+	private function get_css() {
 		$css = [];
 
 		foreach ( $this->elements as $id => $element ) {
@@ -19,9 +39,6 @@ class CSS {
 			}
 		}
 
-		// Only include custom colors in customizer or frontend.
-		if ( is_customize_preview() || $css ) {
-			echo "<style id='estar-colors'>\n:root {\n", esc_html( implode( "\n", $css ) ), "\n}\n</style>\n";
-		}
+		return $css;
 	}
 }
