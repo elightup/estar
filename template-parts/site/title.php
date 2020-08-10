0441973a08f025003b28dfb85e4eb39970311876
LongVu93 <longvt@elightup.com>
2020-08-10 15:05:49 +0700

    Fix h1 ở site-title

--- template-parts/site/title.php
+++ template-parts/site/title.php
@@ -1,4 +1,4 @@
-<?php $html_tag = is_singular() ? 'div' : 'h1'; ?>
+<?php $html_tag = is_front_page() ? 'h1' : 'div'; ?>
 <<?php echo esc_html( $html_tag ) ?> class="site-title">
 	<a href="<?php echo esc_url( home_url() ) ?>"><?php bloginfo( 'name' ) ?></a>
 </<?php echo esc_html( $html_tag ) ?>>
