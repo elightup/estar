34d1118c1f84cb06018632ed62ad0753758e0bb2
Anh Tran <anhtnt@elightup.com>
2020-08-12 10:12:11 +0700

    Show h1 for title only on front page which displays posts

--- template-parts/site/title.php
+++ template-parts/site/title.php
@@ -1,4 +1,4 @@
-<?php $html_tag = is_front_page() ? 'h1' : 'div'; ?>
+<?php $html_tag = is_front_page() && is_home() ? 'h1' : 'div'; ?>
 <<?php echo esc_html( $html_tag ) ?> class="site-title">
 	<a href="<?php echo esc_url( home_url() ) ?>"><?php bloginfo( 'name' ) ?></a>
 </<?php echo esc_html( $html_tag ) ?>>
